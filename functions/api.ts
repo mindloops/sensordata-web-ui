4aec3991eeea0f2c66cc37d90ae27864da2fbf1e
Richard Kettelerij <richard@mindloops.nl>
2025-04-26 23:40:34 +0200

    Add Cloudflare Worker to act as CORS proxy to RIVM STA endpoint

--- functions/api.ts
+++ functions/api.ts
@@ -1,27 +1,14 @@
-const API_URL = 'https://api-samenmeten.rivm.nl/v1.0';
+export async function onRequest(context) {
+  // rewrite the reuqest's URL to a fixed value.
+  // you could also use an environment variable.
+  const apiUrl = new URL("https://api-samenmeten.rivm.nl/v1.0");
 
-export const onRequestOptions = async () => {
-  return new Response(null, {
-    status: 204,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*',
-      'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
-      'Access-Control-Max-Age': '86400',
-    },
-  });
-};
+  const requestUrl = new URL(context.request.url);
+  // remove the /api prefix
+  apiUrl.pathname = requestUrl.pathname.replace(/^\/api/, '');
+  apiUrl.search = requestUrl.search;
 
-export const onRequest = async (context) => {
-  console.log(`[LOGGING FROM /api]: Request came from ${context.request.url}`)
-
-  const immutableResponse = await fetch(
-    new Request(API_URL, context.request),
-  );
-
-  const response = new Response(immutableResponse.body, immutableResponse);
-
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Max-Age', '86400');
-  return response;
+  // proxy the request to the API backend.
+  const request = new Request(apiUrl, context.request);
+  return fetch(request);
 };
